a5006ba5fe990de5a33c7c1c01766aa2d2eb0b9d
Nik Geh <nikgeh@gmail.com>
2011-07-16 23:32:54 -0700

    Created initial API code For Assignment 2. Haven't tested that it works yet

--- CalculatorBrain.h
+++ CalculatorBrain.h
@@ -10,11 +10,26 @@
 
 
 @interface CalculatorBrain : NSObject {
+    @private
     double operand;
     double waitingOperand;
+    NSString *waitingOperation;
+    NSMutableArray *expressionArray;
 }
 
 - (void)setOperand:(double)aDouble;
 - (double)performOperation:(NSString *)operation;
+- (void)setVariableAsOperand:(NSString *)variable;
+
+@property (readonly) id expression;
+
++ (double)evaluateExpression:(id)anExpression
+         usingVariableValues:(NSDictionary *)variables;
+
++ (NSSet *)variablesInExpression:(id)anExpression;
++ (NSString *)descriptionOfExpression:(id)anExpression;
+
++ (id)propertyListForExpression:(id)anExpression;
++ (id)expressionForPropertyList:(id)propertyList;
 
 @end
--- CalculatorBrain.m
+++ CalculatorBrain.m
@@ -9,8 +9,11 @@
 
 #import "CalculatorBrain.h"
 
-@interface CalculatorBrain()
+#define VARIABLE_PREFIX @"%"
 
+@interface CalculatorBrain()
+- (void)performWaitingOperation;
+- (id)initWithBlankExpressions;
 @property (retain) NSString *waitingOperation;
 @end
 
@@ -20,11 +23,63 @@
 // will release any memory associated with it when we set a new value
 @synthesize waitingOperation;
 
-- (void)setOperand:(double)aDouble {
-    operand = aDouble;
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        expressionArray = [[NSMutableArray alloc] initWithObjects:nil];        
+    }
+    return self;
 }
 
-- (void)performWaitingOperation {
+- (id)initWithBlankExpressions
+{
+    self = [super init];
+    if (self) {
+        expressionArray = nil;        
+    }
+    return self;
+}
+
+
+// Returns the expression @property
+- (id)expression 
+{
+    NSMutableArray *copyOfArray = [expressionArray copy];
+    [copyOfArray autorelease];
+    return copyOfArray;
+}
+
+
+- (void)setOperand:(double)aDouble 
+{
+    operand = aDouble;
+    NSNumber *newOperand = [NSNumber numberWithDouble:aDouble];
+    [expressionArray addObject:newOperand];
+}
+
+- (void)setVariableAsOperand:(NSString *)variable 
+{
+    [expressionArray addObject:[NSString stringWithFormat:@"%s%s", VARIABLE_PREFIX, variable]];
+}
+
+- (double)performOperation:(NSString *)operation 
+{
+    if ([operation isEqual:@"sqrt"]) {
+        operand = sqrt(operand);
+    } else if ([@"+/-" isEqual:operation]) {
+        operand = -operand;
+    } else {
+        [self performWaitingOperation];
+        self.waitingOperation = operation;
+        waitingOperand = operand;
+    }
+    [expressionArray addObject:operation];
+    return operand;
+}
+
+- (void)performWaitingOperation 
+{
     if ([@"+" isEqual:self.waitingOperation]) {
         operand = waitingOperand + operand;
     } else if ([@"-" isEqual:self.waitingOperation]) {
@@ -38,22 +93,61 @@
     }
 }
 
-- (double)performOperation:(NSString *)operation {
-    if ([operation isEqual:@"sqrt"]) {
-        operand = sqrt(operand);
-    } else if ([@"+/-" isEqual:operation]) {
-        operand = -operand;
-    } else {
-        [self performWaitingOperation];
-        self.waitingOperation = operation;
-        waitingOperand = operand;
+
++ (double)evaluateExpression:(id)anExpression
+         usingVariableValues:(NSDictionary *)variables 
+{
+    CalculatorBrain *brain = [[CalculatorBrain alloc] initWithBlankExpressions];
+    NSArray *myExpression = (NSArray *)anExpression;
+    for (id token in myExpression) {
+        if ([token class] == [NSString class]) {
+            // This is an operator or variable
+            NSString *tokenString = (NSString *)token;
+            if ([tokenString hasPrefix:VARIABLE_PREFIX]) {
+                [brain setVariableAsOperand:tokenString];
+            } else {
+                [brain performOperation:tokenString];
+            }
+        } else if ([token isKindOfClass:[NSNumber class]]){
+            // This is an operand
+            [brain setOperand:[((NSNumber *)token) doubleValue]];
+        } else {
+            // What do we do here? Throw something?
+        }
     }
-    return operand;
+    double retVal = brain->operand;
+    [brain release];
+    return retVal;
+}
+
++ (NSSet *)variablesInExpression:(id)anExpression
+{
+    // TODO
+    return nil;
+}
+
++ (NSString *)descriptionOfExpression:(id)anExpression 
+{
+    // TODO
+    return nil;
+}
+
++ (id)propertyListForExpression:(id)anExpression
+{
+    // TODO
+    return nil;
+}
+
++ (id)expressionForPropertyList:(id)propertyList
+{
+    // TODO
+    return nil;
 }
 
 - (void)dealloc
 {
     self.waitingOperation = nil;
+    [expressionArray release];
     [super dealloc];
 }
 
